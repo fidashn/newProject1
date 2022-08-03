2134cb2ee85014ea2558c51ef24cd11d604ed6f1
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-03 23:14:36 +0300

    aditted the display of the cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -3,16 +3,18 @@ import React from 'react'
 function Cards() {
   return (
     <div className='cards-wrapper'>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
+      <div className="card">Card 1</div>
+      <div className="card">Card 2</div>
+      <div className="card">Card 3</div>
+      <div className="card">Card 4</div>
+      <div className="card">Card 5</div>
+      <div className="card">Card 6</div>
+      <div className="card">Card 7</div>
+      <div className="card">Card 8</div>
+      <div className="card">Card 9</div>
+      <div className="card">Card 10</div>
+      <div className="card">Card 11</div>
+      <div className="card">Card 12</div>
     </div>
   )
 }
