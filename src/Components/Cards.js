1f9a53ae24a9b6f57e7ce3ba6cc9954f834db94b
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-04 22:37:46 +0300

    started cards interior structure

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -3,18 +3,18 @@ import React from 'react'
 function Cards() {
   return (
     <div className='cards-wrapper'>
-      <div className="card">Card 1</div>
-      <div className="card">Card 2</div>
-      <div className="card">Card 3</div>
-      <div className="card">Card 4</div>
-      <div className="card">Card 5</div>
-      <div className="card">Card 6</div>
-      <div className="card">Card 7</div>
-      <div className="card">Card 8</div>
-      <div className="card">Card 9</div>
-      <div className="card">Card 10</div>
-      <div className="card">Card 11</div>
-      <div className="card">Card 12</div>
+      <div className="card">Card 1 <p>title</p></div>
+      <div className="card">Card 2 <p>title</p></div>
+      <div className="card">Card 3 <p>title</p></div>
+      <div className="card">Card 4 <p>title</p></div>
+      <div className="card">Card 5 <p>title</p></div>
+      <div className="card">Card 6 <p>title</p></div>
+      <div className="card">Card 7 <p>title</p></div>
+      <div className="card">Card 8 <p>title</p></div>
+      <div className="card">Card 9 <p>title</p></div>
+      <div className="card">Card 10 <p>title</p></div>
+      <div className="card">Card 11 <p>title</p></div>
+      <div className="card">Card 12 <p>title</p></div>
     </div>
   )
 }
