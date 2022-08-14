675c6952efac301dedce2018108b63fb24e79678
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-14 22:00:42 +0300

    removed the links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 import SignUp from './Components/SignUp';
 import SignIn from './Components/SignIn';
 import Home from './Components/Home';
@@ -12,10 +12,8 @@ function App() {
       <Navbar />
       <BrowserRouter >
       <Route path="/home" component={Home} />
-      <Link>
       <Route path="/signUp" component={SignUp} />
       <Route exact path="/signIn" component={SignIn} />
-      </Link>
       </BrowserRouter>
       <Footer/>
     </div>
