9aa9417808c731da3ac2974be96548785590669f
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-16 23:31:30 +0300

    changed navbar  items

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,6 @@ function Footer() {
     <li className="nav-item">Customer Service</li>
     <li className="nav-item">Copy Rights &copy;2022 </li>
     </ul>
-    
      </>
   )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,11 +4,11 @@ function Navbar() {
   return (
     <>
    <ul className="nav-list">
-    <li className="nav-item">nav item</li>
-    <li className="nav-item">nav item</li>
-    <li className="nav-item">nav item</li>
-    <li className="nav-item">nav item</li>
-    <li className="nav-item">nav item</li>
+    <li className="nav-item">My Albums</li>
+    <li className="nav-item">Featured Gallery</li>
+    <li className="nav-item">Videos</li>
+    <li className="nav-item">Favorite</li>
+    <li className="nav-item">Trash</li>
     <button className="sign-up">Sign Up</button>
     <button className="sign-in">Sign In</button>
     <div className="hamburger">H</div>
