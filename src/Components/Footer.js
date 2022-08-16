93d62a33feb320356d0076ecedd73f26f4b9f691
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-16 23:29:42 +0300

    changed footer items

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,11 +4,9 @@ function Footer() {
   return (
     <>
     <ul className="footer-list">
-    <li className="nav-item">footer item</li>
-    <li className="nav-item">footer item</li>
-    <li className="nav-item">footer item</li>
-    <li className="nav-item">footer item</li>
-    <li className="nav-item">footer item</li>
+    <li className="nav-item">Support</li>
+    <li className="nav-item">Customer Service</li>
+    <li className="nav-item">Copy Rights &copy;2022 </li>
     </ul>
     
      </>
